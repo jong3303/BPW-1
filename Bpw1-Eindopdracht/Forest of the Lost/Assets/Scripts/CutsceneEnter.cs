146801043935125ecbec72ac2db670cd97449b37
shanice <jong3303@hotmail.com>
2020-06-14 02:22:26 +0200

    reordered files, added music and New fire particle made
    Nieuwe vuur particle gemaakt dmv photoshop file en meerdere particle lagen.
    Script files zijn verplaatst naar script map.
    Ook muziek toegevoegd

---
+++ Bpw1-Eindopdracht/Forest of the Lost/Assets/Scripts/CutsceneEnter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public GameObject cutsceneCam;
+
+    void OnTriggerEnter(Collider other)
+    {
+        cutsceneCam.SetActive(true);
+        thePlayer.SetActive(false);
+    }
+}
