9eb582fe8c208cd82321d4dc18f4a7dd01c58a2c
shanice <jong3303@hotmail.com>
2020-07-05 19:16:04 +0200

    update models and animations

--- Bpw1-Eindopdracht/Forest of the Lost/Assets/Scripts/CutsceneTrigger.cs
+++ Bpw1-Eindopdracht/Forest of the Lost/Assets/Scripts/CutsceneTrigger.cs
@@ -1,17 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CutsceneTrigger : MonoBehaviour
 {
     public bool TriggerScene = false;
+    public PlayableDirector timeline;
+    public FollowPlayer LanternFollow;
+
+    void Start()
+    {
+        timeline = timeline.GetComponent<PlayableDirector>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject);
-        if (other.gameObject.tag == "Carryable")
+        if (other.gameObject.tag == "Carryable" && LanternFollow.PickUp == true)
         {
             Debug.Log("Cutscene is triggered");
+            timeline.Play();
         }
+    }
+    void OnEnable()
+    {
+        timeline.stopped += OnPlayableDirectorStopped;
+    }
+
+    void OnPlayableDirectorStopped(PlayableDirector aDirector)
+    {
+        if (timeline == aDirector)
+        {
+            timeline.enabled = false;
+        }
+    }
+    void OnDisable()
+    {
+        timeline.stopped -= OnPlayableDirectorStopped;
     }
 }
